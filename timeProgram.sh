ac6f56269660d8af5e1aae832e1adf9e8a352027
Kartik Ramesh <kartikx2000@gmail.com>
2021-03-12 19:09:48 +0530

    Remove test. c

--- timeProgram.sh
+++ timeProgram.sh
@@ -1,4 +1,4 @@
-for i in 1 2 3 4 5 6 7 8 9 10
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
 do  
    # echo "Welcome $i times"
    mpiexec -n $i ./exectime pg-being_ernest.txt OR and lol cykablyat
