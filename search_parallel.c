9cec3b74b0d3e915758528eb0685f270fa307062
Kartik Ramesh <kartikx2000@gmail.com>
2021-03-12 11:29:14 +0530

    Send back messages to P0 indicating status of local search

--- search_parallel.c
+++ search_parallel.c
@@ -65,22 +65,38 @@ int main(int argc, char* argv[]) {
 
     int local_n, start_offset;
 
-    char* localWordSet[(int)ceil((float)numWords / size)];
+    int maxLocalSize = (int)ceil((float)numWords / size);
+
+    // Stores the words alloted to this process.
+    char* localWordSet[maxLocalSize];
+
+    // Stores the corresponding global indices into wordSet for each local word.
+    int wordIndex[maxLocalSize];
+
     int localWordCount = 0;
+
+    // Stores total number of processes that have been allotted the same word
+    // as this process. Only useful when each process gets only 1 word.
     int totalProcessesForWord;
 
     // Some processes get more than one word.
     if (size < numWords) {
         for (int i = 0; i < numWords; i++) {
-            if (i % size == rank) localWordSet[localWordCount++] = wordSet[i];
+            if (i % size == rank) {
+                localWordSet[localWordCount] = wordSet[i];
+                wordIndex[localWordCount] = i;
+                localWordCount++;
+            }
         }
 
         // This word is only present in this process.
         // Hence this process will need to search in entire file.
         totalProcessesForWord = 1;
     } else {
-        localWordSet[localWordCount++] = wordSet[rank % numWords];
+        localWordSet[localWordCount] = wordSet[rank % numWords];
+        wordIndex[localWordCount] = rank % numWords;
         totalProcessesForWord = ceil((size - rank % numWords) / (float)numWords);
+        localWordCount++;
     }
 
     if (totalProcessesForWord == 1) {
@@ -91,18 +107,72 @@ int main(int argc, char* argv[]) {
         start_offset = 0 + local_n * (rank / numWords);
     }
 
-    printAllottedWords(localWordSet, localWordCount, totalProcessesForWord, rank, start_offset,
-                       local_n, size);
+    // printAllottedWords(localWordSet, localWordCount, totalProcessesForWord, wordIndex, rank,
+    //                    start_offset, local_n, size);
 
     // printLocalContents(fileContents, start_offset, local_n, rank, size);
 
+    int sendbuf;
+
+    int foundWords[numWords];
+    memset(foundWords, 0, numWords * sizeof(int));
+    int foundCount = 0;
+
     for (int i = 0; i < localWordCount; i++) {
         int local_ans = searchWord(fileContents, numLines, start_offset, local_n, localWordSet[i]);
-
-        if (local_ans == 1) {
+        sendbuf = wordIndex[i];
+        if (local_ans == true) {
             printf("P%d found %s\n", rank, localWordSet[i]);
+
+            if (rank != 0) {
+                MPI_Send(&sendbuf, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                // printf("P%d sent\n", rank);
+            } else {
+                if (foundWords[wordIndex[i]] == 0) {
+                    foundCount += 1;
+                }
+                foundWords[wordIndex[i]] = 1;
+            }
+        } else {
+            // All indices returned in the message should be between [0 .. MAX_WORDSET - 1]
+            // A value greater indicates that the word wasn't found.
+            sendbuf += MAX_WORDSET;
+            if (rank != 0) {
+                MPI_Send(&sendbuf, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                // printf("P%d sent\n", rank);
+            }
         }
     }
+
+    if (rank == 0) {
+        int totalMessages;
+        if (size <= numWords) {
+            totalMessages = numWords - localWordCount;
+        } else {
+            totalMessages = size - 1;
+        }
+
+        // printf("P0 expects %d messages\n", totalMessages);
+
+        int recv;
+        for (int i = 0; i < totalMessages; i++) {
+            MPI_Recv(&recv, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // printf("P0 recvd %d\n", recv);
+            if (recv < MAX_WORDSET && foundWords[recv] == 0) {
+                foundCount += 1;
+                foundWords[recv] = 1;
+            }
+        }
+
+        // printf("Expecting %d, Foundcount is: %d\n", numWords, foundCount);
+
+        if (option == OR && foundCount > 0)
+            printf("Found atleast one word\n");
+        if (option == AND && foundCount == numWords)
+            printf("Found all %d words\n", numWords);
+    }
+
+    // printf("P%d exited\n", rank);
 
     MPI_Finalize();
 }
--- search_parallel.h
+++ search_parallel.h
@@ -1,12 +1,18 @@
 #ifndef SEARCH_PARALLEL_H
 #define SEARCH_PARALLEL_H
 
-#define MAX_LINE 50
+// Size in bytes of a line.
+#define MAX_LINE 1000
+
+// Number of total lines in the file
 #define MAX_FILE 1000
+
+// Number of query words provided.
 #define MAX_WORDSET 10
 
 #define true 1
 #define false 0
+
 #define AND 0
 #define OR 1
 
