f26a8e172affbad6082791d7b46f6b5a88e18b6f
Kartik Ramesh <kartikx2000@gmail.com>
2021-03-16 12:20:03 +0530

    Updating timing scripts

--- timeProgram.sh
+++ timeProgram.sh
@@ -1,5 +1,6 @@
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+# for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+for i in 1 2 3 4 5 6 7 8 9 10
 do  
    # echo "Welcome $i times"
-   mpiexec -n $i ./exectime pg-being_ernest.txt OR and lol cykablyat
+   mpiexec -n $i ./exectime ./BigFile/pg-being_ernest_l1.txt OR ankp bjcy cyka blya
 done
